776afbf859f43adb4489d7715703e8adb8fd33fd
imbroinise99 <imbroinise.davide@iisgalvanimi.edu.it>
2020-05-23 14:03:17 +0000

    api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,12 @@ export class AppComponent {
 
   constructor(public http: HttpClient) {} //dependency injection
 
+  pokemon: Pokemon;
+
   data: Object;
   observable: Observable<Object>;
+
+  obsPost: Observable<Object>;
 
   getData = (d : Object) => {
     this.data = new Object(d);
@@ -24,4 +29,12 @@ export class AppComponent {
     this.observable.subscribe(this.getData); //ci sottoscriviamo all'observable
     return false;
   }
+
+  catturaPokemon(): boolean {
+    let newData = new Pokemon();
+    this.obsPost = this.http.post('', newData);
+    this.obsPost.subscribe(data => this.data = data);
+    return false;
+  }
+
 }
---
+++ client/src/app/pokemon.ts
@@ -0,0 +1,4 @@
+export class Pokemon{
+  nome: string;
+  url: string;
+}
